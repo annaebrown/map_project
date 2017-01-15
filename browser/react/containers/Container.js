65ad85730dd9229c74b7ce7aeffed5c7b3226a42
annaebrown <annabrown4789@gmail.com>
2017-01-15 10:43:24 -0500

    sintegrated other library

--- browser/react/containers/Container.js
+++ browser/react/containers/Container.js
@@ -2,14 +2,14 @@ import React, {Component} from 'react';
 // import {GoogleApiWrapper} from 'google-maps-react/dist/index';
 // import MapComponent from '../components/Map';
 // import Marker from '../components/Marker';
-import NewMapComponent from '../components/NewMapComponent'
+import SimpleMapPage from '../components/NewMapComponent'
 
 export class Container extends Component {
   
 	render() {
 
     return (
-      		<NewMapComponent/>
+      		<SimpleMapPage/>
     )
   }
 }
